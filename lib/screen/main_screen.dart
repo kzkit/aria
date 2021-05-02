477e40da47a8cb0e40a0e43fd92058b272241ac6
Zhen Kit <zhenkit1996@gmail.com>
2021-05-03 00:08:42 +0800

    added new background image

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,19 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:aria/widgets/city_selection.dart';
 import 'package:aria/widgets/forecast.dart';
 import 'package:aria/widgets/weather.dart';
-import 'package:flutter/material.dart';
+import 'package:aria/widgets/weather_details.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+        child: Stack(
           children: [
-            Weather(),
-            CitySelection(),
-            Forecast(),
+            Column(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * .34,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/city_background.png'),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Weather(),
+                CitySelection(),
+                Forecast(),
+                WeatherDetails(),
+              ],
+            )
           ],
         ),
       ),
